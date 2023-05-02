795c4bacc9cc3d52b41f64e87f48ffd6e8fe8b30
5jiji <60753482+5jiji@users.noreply.github.com>
2023-05-02 22:37:25 +0200

    added a base config

---
+++ src/base-config.ts
@@ -0,0 +1,12 @@
+// Don't forget to copy this file as config.ts!
+
+export const bot = {
+  token: "The token of the bot",
+  beta: true
+}
+
+export const sequelize = {
+  database: "The database name",
+  username: "The username of the mysql database",
+  password: "A password for the user",
+}
